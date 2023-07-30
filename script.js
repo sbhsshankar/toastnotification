c8ad71ed31ee6be1a561eb85a59bbf606018bc95
Sbhss <shankarpulaparthi1216@gmail.com>
2023-07-30 12:47:21 +0530

    files added

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let toastBox = document.getElementById('toastBox');
+let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitted';
+let invalidMsg = '<i class="fa-solid fa-circle-xmark"></i> Invalid input, check again';
+let errorMsg = '<i class="fa-solid fa-circle-exclamation"></i> Please fix the error';
+
+function showToast(msg){
+    let toast = document.createElement('div');
+    toast.classList.add('toast')
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+    if(msg.includes('error')){
+        toast.classList.add('error');
+    }
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid');
+    }
+
+    setTimeout(()=>{
+        toast.remove();
+    },6000)
+
+}
